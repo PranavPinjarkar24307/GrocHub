bf0dc40df24a55df30b8bea1bac0ff3ecd075186
Pranav <pranavpinjarkar240307@>
2025-11-28 21:30:48 +0530

    Login Page , Splashscreen , welcome

---
+++ app/src/main/java/com/example/grochub/Splashscreen.java
@@ -0,0 +1,32 @@
+package com.example.grochub;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        VideoView videoView = findViewById(R.id.splashVideo);
+
+        // Load video from raw folder
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splashscreen);
+        videoView.setVideoURI(video);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                startActivity(new Intent(Splashscreen.this, WelcomePage.class));
+                finish();
+            }
+        });
+
+        videoView.start();
+    }
+}
